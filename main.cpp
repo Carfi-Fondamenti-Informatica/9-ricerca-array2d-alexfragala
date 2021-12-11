de366d20f569b4cd755de926b30ce05f978b44db
alexfragala <91506326+alexfragala@users.noreply.github.com>
2021-12-11 21:21:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+
+using namespace std;
+
+
+int main() {
+    int b=0;
+    char A[10][20], B[20];
+    for(int i=0; i<10; i++){
+            cin >> A[i];
+    }
+        cin >> B;
+    if (posizione(A,B,b)){
+        cout << b << endl;
+    }else{
+        cout << "non presente" << endl;
+    }
+
+    return 0;
 }
