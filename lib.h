b8d226eaa4eca7736b3c352c1bf8431a359d4aaa
alexfragala <91506326+alexfragala@users.noreply.github.com>
2021-12-11 21:21:23 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by Alex Fragalà on 09/12/21.
+//
+
+#ifndef ESERCITAZIONE9_LIB_H
+#define ESERCITAZIONE9_LIB_H
+#define LUNGHEZZA_MAX 100
+bool posizione(char A[10][20], char B[10], int &b);
+#endif //ESERCITAZIONE9_LIB_H
 
